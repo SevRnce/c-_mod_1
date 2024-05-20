60fb86c9d47fd76aacbd9b63e1fc239bee8cf0a7
SevRnce <90710004+SevRnce@users.noreply.github.com>
2024-05-19 21:35:48 -0600

    Add files via upload

---
+++ info.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string first = "George";
+	string last = "Jungle";
+	string address = "1234 of the Dr.";
+	string city = "Austin";
+	int zip = 54321;
+	cout << "First Name: " << first << "\nLast Name: " << last  << "\nAddress: " << address << "\nCity: " << city << "\nZip: " << zip;
+}
+
+
+
+
